dae7d36f14c7f91dfbf340bab3fe54b40bd7885f
kokhant99 <prowebdeveloper2021@gmail.com>
2022-05-24 23:15:01 +0630

    third day

--- admin/cat-update.php
+++ admin/cat-update.php
@@ -3,7 +3,7 @@
     $id = $_POST['id'];
     $name = $_POST['name'];
 
-    $sql = "UPDATE categories SET name=$name modified_date=now() WHERE id=$id";
+    $sql = "UPDATE categories SET name='$name', modified_date=now() WHERE id='$id'";
     mysqli_query($conn, $sql);
 
     header("location:cat-list.php");
